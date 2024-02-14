2086bfc5a610d62b20618cd1f2fdecc2458f768c
manga <manga@DESKTOP-9FI0ELA>
2024-02-13 19:05:48 -0500

    Add project files.

---
+++ TaskManager/Controllers/TaskController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TaskManager.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class TaskController : Controller
+    {
+        private readonly ILogger<TaskController> _logger;
+        public TaskController(ILogger<TaskController> logger)
+        {
+            _logger = logger;
+        } 
+
+
+    }
+}
